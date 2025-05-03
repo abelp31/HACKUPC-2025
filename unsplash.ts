29dcde22761c66a8a9c92f729d0edfb68a65dce0
Sergi Vos <sergi@wikiloc.com>
2025-05-03 19:55:02 +0200

    misc

--- unsplash.ts
+++ unsplash.ts
@@ -1,6 +1,12 @@
 const UNSPLASH_API_KEY = process.env.UNSPLASH_API_KEY;
 
+const cache = new Map<string, string>();
+
 export const getUnsplashImages = async (query: string) => {
+    if (cache.has(query)) {
+        return cache.get(query);
+    }
+
     const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&page=1&per_page=1&orientation=landscape`, {
         method: 'GET',
         headers: {
@@ -13,10 +19,18 @@ export const getUnsplashImages = async (query: string) => {
         return [];
     }
     const data = await response.json();
-    return data.results[0]?.urls?.regular || "https://http.cat/images/202.jpg";
+
+    const imageUrl = data.results[0]?.urls?.regular || "https://http.cat/images/202.jpg";
+    cache.set(query, imageUrl);
+
+    return imageUrl;
 }
 
 export const getWikipediaImage = async (query: string) => {
+    if (cache.has(query)) {
+        return cache.get(query);
+    }
+
     //  Query https://en.wikipedia.org/w/api.php?action=query&titles=Kyoto&prop=pageimages&format=json&pithumbsize=1024
     const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&titles=${query}&prop=pageimages&format=json&pithumbsize=1024`, {
         method: 'GET',
@@ -31,5 +45,9 @@ export const getWikipediaImage = async (query: string) => {
     const data = await response.json();
     const pages = data.query.pages;
     const pageId = Object.keys(pages)[0];
-    return pages[pageId]?.thumbnail?.source || "https://http.cat/images/202.jpg";
+
+    const imageUrl = pages[pageId]?.thumbnail?.source || "https://http.cat/images/202.jpg";
+    cache.set(query, imageUrl);
+
+    return imageUrl;
 }
