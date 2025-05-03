afdb1e8901ea5bf3584cdeaf6dfe6b78e425c255
Sergi Vos <sergi@wikiloc.com>
2025-05-03 02:06:23 +0200

    misc

--- main.js
+++ main.js
@@ -1,18 +1,44 @@
-const express = require('express');
+import express from 'express';
+import { generateContent } from './question_generator.js';
+
 const app = express();
 const port = 3000;
+
+let rooms = []
 
 // Middleware para parsear el cuerpo de la solicitud como JSON
 app.use(express.json());
 
+
+// inputs: {questions: "text que utilitzarem per generar les preguntes+respostes"}
+// outputs: {code: "12345"}
 app.post('/create-session', (req, res) => {
-    const { username, num_participants } = req.body;
-    if (username && num_participants) {
-        //create game and return game_code
-        res.status(200).json({ message: 'Session created with code: ' });
+    const { questions } = req.body;
+    if (questions) {
+        // Generate a 6 digit/character code, mix of 3 letters and then 3 numbers
+        const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        const numbers = '0123456789';
+        const randomLetters = Array.from({ length: 3 }, () => letters.charAt(Math.floor(Math.random() * letters.length))).join('');
+        const randomNumbers = Array.from({ length: 3 }, () => numbers.charAt(Math.floor(Math.random() * numbers.length))).join('');
+        const roomCode = randomLetters + randomNumbers;
+        res.status(200).json({ code: roomCode });
+    } else {
+        res.status(400).json({ error: 'Questions can not be null' });
     }
-    else if (!num_participants){
-        res.status(400).json({ error: 'Number of participants can not be null' });
+});
+
+app.get("/test", async (req, res) => {
+    // return response.text of generateContent, use await
+    const response = await generateContent();
+    res.status(200).json({ text: response.text });
+});
+
+app.post("/start-game", (req, res) => {
+    const { gameCode } = req.body;
+    if (gameCode) {
+        res.status(200).json({ message: 'Game started' });
+    } else {
+        res.status(400).json({ error: 'Game code can not be null' });
     }
 });
 
@@ -24,19 +50,9 @@ app.post('/start-session', (req, res) => {
         //add user to test and summ number of users joined
         //if user was last user start game
         res.status(200).json({ message: `${username} joined, waiting for more users` });
-        res.status(400).json({ message: `${username} joined, waiting for more users` });
     } else {
-        
+
     }
-});
-
-app.post('/', (req, res) => {
-});
-
-app.post('/', (req, res) => {
-});
-
-app.post('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
